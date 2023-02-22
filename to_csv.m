2498b85ca02e60fe720fd39dcfed20c867120d14
Ali Mohammadi <65521817+ali-mohammadi-scrc@users.noreply.github.com>
2023-02-22 15:50:39 +0100

    Update to_csv.m

--- to_csv.m
+++ to_csv.m
@@ -1,12 +1,13 @@
-root_dir = 'F:\ScRc\Desktop\New folder';
-Files = dir([root_dir, '\**\*.mat']);
+root_dir = 'D:\Users\Windows\Desktop';
+Files = dir([root_dir, '\*.mat']);
 
 for i = 1:length(Files)
     folder = Files(i).folder;
     name = Files(i).name;
     load([folder, '\', name]);
-    intervals = Calls.Box(:, [1, 3]);
+    intervals = Calls.Box(:, [1, 3, 2, 4]);
     intervals(:, 2) = intervals(:, 1) + intervals(:, 2);
+    intervals(:, 4) = intervals(:, 3) + intervals(:, 4);
     name = split(name);
     name = name{1};
     save_path = [folder, '\deepsqueak', name(4:end), '.csv'];
